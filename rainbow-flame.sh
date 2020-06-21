48549250d7c53c6b506bb410cf9759df959fcac5
WovenCoast <wovencoast14782@techie.com>
2020-06-21 19:24:10 +0500

    Ultra fancy eh?

--- rainbow-flame.sh
+++ rainbow-flame.sh
@@ -1,11 +1,38 @@
-while :
-do
-    git pull
-    npm run build
-    npm run start
-    echo ""
-    echo "Process exited by itself, waiting for 5s, use Ctrl + C to exit the process"
-    sleep 5s
-    echo "Restarting..."
-    echo ""
-done
+#!/bin/bash
+bot() {
+    while :
+    do
+        git pull
+        echo "Bot: Pulled from git"
+        npm run build
+        echo "Bot: Built typescript bot"
+        npm run start
+        echo ""
+        echo "Bot: Process exited by itself, waiting for 5s, use Ctrl + C to exit the process"
+        sleep 5s
+        echo "Bot: Restarting..."
+        echo ""
+    done
+}
+
+dashboard() {
+    while :
+    do
+        git pull
+        echo "React: Pulled from git"
+        cd dashboard
+        npm run build
+        echo "React: Built react dashboard"
+        cd ..
+        PORT=5432 serve dashboard/build
+        echo ""
+        echo "React: Process exited by itself, waiting for 5s, use Ctrl + C to exit the process"
+        sleep 5s
+        echo "React: Restarting..."
+        echo ""
+    done
+}
+
+bot &
+dashboard &
+wait
